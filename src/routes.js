701f26da3e6575da796da56df58f25e5df4bb218
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-20 00:47:19 -0300

    User error solved (User not Appearing in response store)

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 import { Router } from 'express'
+import HouseController from './controllers/HouseController'
 import sessionController from './controllers/SessionController'
 import FromController from './controllers/SessionController'
+import multer from 'multer'
+import uploadConfig from './config/upload'
 
 const routes = new Router()
+
+const upload = multer(uploadConfig)
 
 routes.post('/sessions', sessionController.store)
 
 routes.get('/sessions', sessionController.index)
+
+routes.post('/houses', upload.single('thumbnail'), HouseController.store)
 
 export default routes
 
