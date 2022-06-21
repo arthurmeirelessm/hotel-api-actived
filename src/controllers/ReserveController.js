47c4b9ad5e1459a9ee679038c0980282fab41b35
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-21 02:16:44 -0300

    Testing reserveStore after add validations in storie method

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -1,5 +1,6 @@
 import Reserve from "../models/Reserve";
-
+import House from "../models/House"
+import User from "../models/User"
 
 
 class ReserveController {
@@ -7,17 +8,38 @@ class ReserveController {
         const { user_id } = req.headers
         const { house_id } = req.params
         const { date } = req.body
-        
-        const CreateReserve = await Reserve.create({
+
+
+        console.log(house_id)
+        const createReserve = await Reserve.create({
             user: user_id,
             house: house_id,
             date,
         })
 
-        return res.json(CreateReserve)
 
+        const getHouse = await House.findById(house_id)
+        const getUser = await User.findById(user_id)
+
+        if (String(getHouse.user) != String(getUser._id)) {
+            return res.status(404).json({ Error: 'House not found' })
+        }
+
+        if (String(getHouse.user) == String(getUser._id)) {
+            return res.status(404).json({ Error: 'User is same that was create' })
+        }
+
+        if (getHouse.status == false) {
+           return res.status(404).json({ Error: 'House not available' })
+        }
+
+
+
+        //Populate: Serve para mesclar os responses ue vem dois dois models citados abaixo (user, house)
+        await createReserve.populate('house').populate('user').execPopulate()
+
+        return res.json(createReserve)
     }
-
 }
 
 
